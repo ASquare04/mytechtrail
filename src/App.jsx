17dcc3f2e9cab28a4d0be12e57590ba41bd29d24
Anmol Arora <heymailme04@gmail.com>
2024-04-27 18:43:57 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="bg-black h-screen flex items-center justify-center">
+        <h1 className="text-9xl text-purple-400">Vite + React + Tailwind</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
