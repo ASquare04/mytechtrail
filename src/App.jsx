4b903277dfaacf387888c958c2f50d3c38841719
Anmol Arora <heymailme04@gmail.com>
2024-05-08 13:10:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Navbar from './components/navbar.jsx'
+
 const App = () => {
   return (
     <>
-      <div className="bg-black h-screen flex items-center justify-center">
-        <h1 className="text-9xl text-purple-400">Vite + React + Tailwind</h1>
-      </div>
+
     </>
   );
 };
