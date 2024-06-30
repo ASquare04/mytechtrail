4a2b489cbb918fce7c6ca9af5e44bf2a2060551b
Anmol Arora <heymailme04@gmail.com>
2024-06-30 13:13:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import Navbar from './components/navbar.jsx'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Homepage from './pages/Homepage';
+import Aboutpage from './pages/Aboutpage';
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<Aboutpage />} />
+      </Routes>
+    </Router>
   );
 };
 
