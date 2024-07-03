dda82580c0c194b66f7f34aa679bb07685394246
Anmol Arora <heymailme04@gmail.com>
2024-07-03 10:42:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+  import React from 'react'
+  import ReactDOM from 'react-dom/client'
+  import App from './App.jsx'
 
-import './index.css'
-import './public/script.js'
+  import './index.css'
+  import './public/script.js'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    // <React.StrictMode>
+      <App />
+    {/* </React.StrictMode>, */}
+  )
