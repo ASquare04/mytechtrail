6769c9f07dd194017c11446d4b65ab6bbf03093a
Anmol Arora <heymailme04@gmail.com>
2024-07-03 12:54:58 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import me from "../public/me.jpg";
 function Hero() {
   return (
     <section class="text-gray-600 body-font">
       <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-        <img
-          class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 rounded"
-          alt="hero"
-          src={me}
-        />
         <div class="text-center lg:w-2/3 w-full">
           <h1 class="title-font font-serif font-extralight md:text-4xl text-lg mb-4 text-white">
             last but not least, i wanna thank me..
