fcfc9c3f50a8cea451c310cbbb233b2a2b0a55e9
Anmol Arora <heymailme04@gmail.com>
2024-07-03 10:15:17 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,35 @@
 import React from "react";
-
 function Hero() {
   return (
     <section class="text-gray-600 body-font">
       <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-        <img
-          class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
-          alt="hero"
-          src="https://dummyimage.com/720x600"
-        />
         <div class="text-center lg:w-2/3 w-full">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Microdosing synth tattooed vexillologist
+          <h1 class="title-font font-serif font-extralight t sm:text-4xl text-3xl mb-4 text-white">
+            last but not least, i wanna thank me..
           </h1>
-          <p class="mb-8 leading-relaxed">
-            Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing
-            tousled. Chambray dreamcatcher trust fund, kitsch vice godard
-            disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh.
-            Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo
-            booth af fingerstache pitchfork.
-          </p>
+
+          <blockquote class="text-xl  italic font-semibold text-gray-900 dark:text-white">
+            <svg
+              class="w-8 h-8 text-white mb-4"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 18 14"
+            >
+              <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+            </svg>
+            <p className="m-6">
+              Let me win. But if I cannot win, let me brave in the attempt. As we know, the only place where success comes before work is in the dictionary.
+            </p>
+            <p className="m-8">Try to be <span className="text-yellow-500">#404</span> sometimes, not always the <span className="text-purple-500">#200</span></p>
+          </blockquote>
+
           <div class="flex justify-center gap-4">
-            <button class="flex items-center px-4 py-2 font-semibold bg-white rounded text-black">
+            <button class="flex items-center px-4 py-2 font-semibold bg-white rounded text-black hover:bg-black transition-all ease-in-out duration-500 hover:text-white">
               <i class="fab fa-github mr-2"></i>
               Just Visit
             </button>
-            <button class="flex items-center px-4 py-2 font-semibold text-white bg-blue-600 rounded hover:bg-blue-700">
+            <button class="flex items-center px-4 py-2 font-semibold text-white bg-blue-600 rounded hover:bg-black  hover:text-blue-600 transition-all ease-in-out duration-500">
               <i class="fab fa-linkedin mr-2"></i>
               Connect Now
             </button>
