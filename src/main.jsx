589151f16a43e8c9c8d01a16de423940ae3800f6
Anmol Arora <heymailme04@gmail.com>
2024-07-04 12:56:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
   import React from 'react'
   import ReactDOM from 'react-dom/client'
   import App from './App.jsx'
-
+  
+  import './public/Fonts/WEB/css/satoshi.css'
   import './index.css'
   import './public/script.js'
 
   ReactDOM.createRoot(document.getElementById('root')).render(
-    
+    // <React.StrictMode>
       <App />
-
+    // </React.StrictMode>,
   )
+  
