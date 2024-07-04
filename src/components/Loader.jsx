adb608be4f4f98df758d0f6fa412120bc848249e
Anmol Arora <heymailme04@gmail.com>
2024-07-04 14:33:12 +0530

    Add files via upload

---
+++ src/components/Loader.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div>
+      <div class="loader">
+        <div class="square" id="sq1"></div>
+        <div class="square" id="sq2"></div>
+        <div class="square" id="sq3"></div>
+        <div class="square" id="sq4"></div>
+        <div class="square" id="sq5"></div>
+        <div class="square" id="sq6"></div>
+        <div class="square" id="sq7"></div>
+        <div class="square" id="sq8"></div>
+        <div class="square" id="sq9"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
