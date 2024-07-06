b322821c25e5184306bd33b5a9a1eed93bb7290b
Anmol Arora <heymailme04@gmail.com>
2024-07-06 13:56:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Homepage from './pages/Homepage';
 import Blogpage from './pages/Blogpage';
+import Skillspage from './pages/Skillspage';
 import Contactpage from './pages/Contactpage';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/blog" element={<Blogpage />} />
+        <Route path="/skills" element={<Skillspage />} />
         <Route path="/connect" element={<Contactpage />} />
       </Routes>
     </Router>
