22eb84a431f2e40adaff5202df0224c6515500b4
Anmol Arora <heymailme04@gmail.com>
2024-07-10 12:59:51 +0530

    Add files via upload

---
+++ src/pages/Projectpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Projectpage = () => {
+  return (
+    <div className='h-screen text-white flex justify-center items-center text-4xl'>
+      Coming Soon
+    </div>
+  )
+}
+
+export default Projectpage
