7f3db695203390abdc83ac4b1dfd044bbc07da74
Anmol Arora <heymailme04@gmail.com>
2024-07-11 18:39:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Homepage from './pages/Homepage';
-import Projectpage from './pages/Projectpage';
-import Blogpage from './pages/Blogpage';
-import Skillspage from './pages/Skillspage';
-import Contactpage from './pages/Contactpage';
+const Loader= lazy(() => import('./components/Loader'));
+const Homepage = lazy(() => import('./pages/Homepage'));
+const Projectpage = lazy(() => import('./pages/Projectpage'));
+const Blogpage = lazy(() => import('./pages/Blogpage'));
+const Skillspage = lazy(() => import('./pages/Skillspage'));
+const Contactpage = lazy(() => import('./pages/Contactpage'));
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/projects" element={<Projectpage />} />      
-        <Route path="/blog" element={<Blogpage />} />
-        <Route path="/skills" element={<Skillspage />} />
-        <Route path="/connect" element={<Contactpage />} />
-  
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/projects" element={<Projectpage />} />      
+          <Route path="/blog" element={<Blogpage />} />
+          <Route path="/skills" element={<Skillspage />} />
+          <Route path="/connect" element={<Contactpage />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
